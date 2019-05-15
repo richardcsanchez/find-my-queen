6062fd72ac575c6c71f6b5429e4b11e221b5bd6d
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-15 15:30:11 -0400

    build drag queen index and show route

--- app/controllers/drag_queens_controller.rb
+++ app/controllers/drag_queens_controller.rb
@@ -1,2 +1,12 @@
 class DragQueensController < ApplicationController
+
+  def index
+    @drag_queens = DragQueen.all
+    render json: @drag_queens
+  end
+
+  def show
+    @drag_queen = DragQueen.find(params[:id])
+    render json: @drag_queen
+  end
 end
