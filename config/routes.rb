5a3bcf84859c006935569b5886a47186f1ac04d1
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-15 14:57:16 -0400

    add style resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :styles
   resources :drag_queens
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
