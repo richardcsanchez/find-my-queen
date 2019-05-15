6088a1dada521a77832280b0b0cded2c5c8129cb
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-15 14:59:03 -0400

    create city resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cities
   resources :styles
   resources :drag_queens
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
