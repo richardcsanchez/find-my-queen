8048fe9e6c4e2e317dbc65a5621ce7e509a1de3a
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-15 16:09:57 -0400

    build out grag_queen CRUD controller routes

--- app/controllers/drag_queens_controller.rb
+++ app/controllers/drag_queens_controller.rb
@@ -1,4 +1,5 @@
 class DragQueensController < ApplicationController
+  before_action :find_queen, only: [:show, :update, :destroy]
 
   def index
     @drag_queens = DragQueen.all
@@ -9,4 +10,41 @@ class DragQueensController < ApplicationController
     @drag_queen = DragQueen.find(params[:id])
     render json: @drag_queen
   end
-end
+
+  def create
+    @drag_queen = DragQueen.new(drag_queen_params)
+
+    if @drag_queen.save
+      render json: @drag_queen
+    else
+      render json @drag_queen.errors
+    end
+  end
+
+  def update
+    if @drag_queen.update(drag_queen_params)
+      render json: @drag_queen
+    else
+      render json: @drag_queen.errors
+    end
+  end
+
+  def destroy
+    if @event.destroy
+      render json: {message: "Bye Queen!"}, status: 204
+    else
+      render json: {message @drag_queen.errors}, status:400
+    end
+  end
+
+  private
+
+    def find_queen
+      @drag_queen = DragQueen.find(params[:id])
+    end
+
+    def drag_queen_params
+      params.require(:drag_queen).permit(:name, :city, :styles, :img_url, :bio)
+    end
+
+  end
