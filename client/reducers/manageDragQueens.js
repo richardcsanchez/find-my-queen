450dc2f3342f3fdda6b024c686fa22137034eb2b
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-20 13:59:33 -0400

    begin create drag queen reducer

---
+++ client/reducers/manageDragQueens.js
@@ -0,0 +1,11 @@
+export default function manageDragQueens(state ={
+    drag_queens: [],
+  }, action) {
+    switch (action.type) {
+      case 'ADD_DRAG_QUEEN' {
+        
+
+      default:
+      return state;
+    }
+  }
