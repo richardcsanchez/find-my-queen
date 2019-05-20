98f6a1f05e84dea5485b32072a09ceecfbb0471c
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-20 17:13:11 -0400

    build drag queen reducer

--- client/reducers/manageDragQueens.js
+++ client/reducers/manageDragQueens.js
@@ -1,9 +1,13 @@
 export default function manageDragQueens(state ={
-    drag_queens: [],
+    dragQueens: [],
   }, action) {
     switch (action.type) {
-      case 'ADD_DRAG_QUEEN' {
-        
+      case 'GET_DRAG_QUEENS':
+        return {...state, dragQueens: action.dragQueens};
+      case 'ADD_DRAG_QUEEN' :
+        return {dragQueens: [...state.dragQueens, action.dragQueen]}
+      case 'REMOVE_DRAG_QUEEN':
+        return {dragQueens: state.dragQueens.filter(dragQueen => dragQueen.id !== action.dragQueen)}
 
       default:
       return state;
