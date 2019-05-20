2beb4e925739fb200436658405593dddafac436a
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-20 17:14:23 -0400

    update dragqueen.js

--- client/components/DragQueen.js
+++ client/components/DragQueen.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export const DragQueen = (props) => {
-  const ingredients = props.drag_queen.
 
+  return(
+    <div>
+      <h2>{props.dragQueen.name}</h2>
+      <br>
+      <img alt={props.dragQueen.name} src={props.dragQueen.img_url}>
+      <br>
+      <p>{props.dragQueen.city}</p>
+      <p>{props.dragQueen.style}</p>
+      <p>{props.dragQueen.bio}</p>
+      </div>
+  )
 }
