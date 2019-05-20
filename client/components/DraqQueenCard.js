96ac7f74b3fc5aacb4dfb42e1b0f45d179b84f13
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-20 17:15:42 -0400

    add image card for drag queens

---
+++ client/components/DraqQueenCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const DragQueenImageCard = (props) => {
+  const drag_queen_url = '/drag_queens/${props.dragQueen.id}'
+
+  return (
+    <div>
+      <Link to={drag_queen_url}
+        <img src={props.dragQueen.img_url} href={drag_queen_url} alt={this.props.name}/>
+      </Link>
+      </div>
+  )
+}
