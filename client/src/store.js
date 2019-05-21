df831a8419121c73fceec5be21fd5ac6956d83d9
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-21 18:47:07 -0400

    remove unecessary arguments to createStore

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,7 @@ const reducers = combineReducers({
 
 const middleware = [thunk]
 
-let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+let store = createStore(reducers,
   applyMiddleware(...middleware)
 );
 
