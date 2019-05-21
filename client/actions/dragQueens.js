a31b4fea6ae4b0c11c219bebb1a4e73f51fafaca
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-21 15:16:20 -0400

    build dragQueen actions file

--- client/actions/dragQueens.js
+++ client/actions/dragQueens.js
@@ -1,1 +1,61 @@
+import { resetEventForm } from './eventForm';
 
+export const setDragQueens = dragQueens => {
+  return {
+    type: 'GET_DRAG_QUEENS',
+    dragQueens
+  }
+}
+
+export const addDragQeen = dragQueen => {
+  return {
+    type:'ADD_DRAG_QUEEN'
+    dragQueen
+  }
+}
+
+export const removeDragQueen = dragQueen => {
+  return {
+    type: 'REMOVE_DRAG_QUEEN',
+    dragQueen: dragQueen
+  }
+}
+
+export const getDragQueens = () => {
+  return dispatch => {
+    const url = 'api/drag_queens'
+    return fetch(url, {
+      method: 'GET,'
+    })
+    .then(res => res.json())
+    .then(dragQueens => {
+      dispatch(setDragQueens(dragQueens))
+    })
+  }
+}
+
+
+export const createDragQueen = dragQueen => {
+  return dispatch => {
+    return fetch('api/drag_queens', {
+      method: 'POST',
+      body: JSON.stringify({dragQueen: dragQueen})
+    })
+    .then(response => response.json())
+    .then(dragQueen => {
+      dispatch(addDragQueen(dragQueen))
+      dispatch(resetEventForm())
+    })
+  }
+}
+
+export const deleteDragQueen = (dragQueenId) => {
+  return dispatch => {
+    return fetch(`api/drag_queens/${dragQueenId}`, {
+      method: "DELETE",
+    })
+    .then(response => {
+      dispatch(removeDragQueen(dragQueenId))
+    })
+  }
+}s
