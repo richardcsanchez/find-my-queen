c3fe0e5cf3fd18b1df05cd56531d25412c7fdb6f
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 13:47:06 -0400

    rename props and export

--- client/src/components/DragQueen.js
+++ client/src/components/DragQueen.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { setDragQueens } from '../actions/dragQueens'
 
 export const DragQueen = (props) => {
 
+
   return(
     <div>
-      <h2>{props.dragQueen.name}</h2>
-      <br>
-      <img alt={props.dragQueen.name} src={props.dragQueen.img_url}>
-      <br>
-      <p>{props.dragQueen.city}</p>
-      <p>{props.dragQueen.style}</p>
-      <p>{props.dragQueen.bio}</p>
+    <h2>Drag Queens:</h2>
+      <h2>{props.name}</h2>
+      <br></br>
+      <img alt={props.name} src={props.img_url}/>
+      <br></br>
+      <p>{props.city}</p>
+      <p>{props.style}</p>
+      <p>{props.bio}</p>
       </div>
   )
 }
+
+export default DragQueen;
