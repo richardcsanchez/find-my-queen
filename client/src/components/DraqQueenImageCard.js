bb7de24378ec232adcba58b67e9fb5f0144b7324
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 13:42:27 -0400

    renamed drag queen card file

--- client/src/components/DraqQueenImageCard.js
+++ client/src/components/DraqQueenImageCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const DragQueenImageCard = (props) => {
-  const drag_queen_url = '/drag_queens/${props.dragQueen.id}'
+  const drag_queen_url = '/drag_queens/${props.id}'
 
   return (
     <div>
