b092a19dfd9d47ca879c38ab8790e8fc0fc1ad76
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 13:47:40 -0400

    create new container to show drag queens

---
+++ client/src/containers/dragQueensContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import DragQueen from './components/DragQueen'
+import { setDragQueens } from './actions/dragQueens'
+
+class DragQueensContainer extends Component {
+
+  render(){
+    return(
+      <div>
+        {this.setDragQueens()}
+        </div>
+      )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    dragQueens: state.dragQueens
+  }
+}
+
+export default connect(mapStateToProps)(DragQueensContainer)
