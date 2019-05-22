4b0bf030ac023212f06e576dc30150aa2b6d606b
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 19:01:59 -0400

    create DragQueenCard

---
+++ client/src/components/DragQueenCard.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+
+const DragQueenCard = ({ dragQueen }) => {
+  return(
+    <div>
+    <h2>{dragQueen.name}</h2>
+    <img alt={ dragQueen.name } src= {dragQueen.img_url} />
+    <p>{dragQueen.hometown}</p>
+    <p>{dragQueen.style}</p>
+    <p>{dragQueen.bio}</p>
+    </div>
+)
+}
+
+export default DragQueenCard
