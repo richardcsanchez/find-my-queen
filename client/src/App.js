c3e45282fccdf9442857cd8698b7581aaedcd832
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 13:40:46 -0400

    update import and render

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux'
 import CreateDragQueen from './components/CreateDragQueen'
-
+import DragQueen from './components/DragQueen'
 
 import manageDragQueens from './reducers/manageDragQueens';
 
@@ -12,8 +12,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        Find My Queen!
         <CreateDragQueen />
+        <DragQueen />
       </div>
     );
   }
