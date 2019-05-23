f9516e93d781035a09bac2a155e240e25bb9aa8f
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 20:38:29 -0400

    update imports and refactor

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -5,35 +5,18 @@ import { connect } from 'react-redux'
 import DragQueenForm from '../containers/DragQueenForm'
 import DragQueens from '../containers/DragQueens'
 import DragQueensContainer from './dragQueensContainer'
-import dragQueens from '../actions/dragQueens'
 import manageDragQueens from '../reducers/manageDragQueens';
 
 
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      dragQueens: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3001/api/drag_queens')
-    .then(res => res.json())
-    .then(dragQueens => {
-      this.setState({ dragQueens })
-    })
-  }
-
   render() {
     console.log(this.state)
     return (
       <div className="App">
         Find My Queen!
-        <DragQueens dragQueens={this.state.dragQueens}/>
+        <DragQueens />
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 
-import store from './store';
+import store from './store.js';
 
 
 import App from './containers/App';
