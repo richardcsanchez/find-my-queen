6a8ba5668a76d55af8cb17b30b34530a32116581
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 20:36:28 -0400

    uxpand and rename drag queen form reducer

--- client/src/reducers/dragQueenFormData.js
+++ client/src/reducers/dragQueenFormData.js
@@ -10,6 +10,8 @@ export default (state = initialState, action) => {
     switch(action.type) {
         case 'RESET_FORM':
             return initialState;
+        case 'UPDATED_DATA':
+            return action.dragQueenFormData
 
         default:
             return state;
