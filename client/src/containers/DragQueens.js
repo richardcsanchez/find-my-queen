9eb2afdd5628e85b62ac3e0357728c02d3b39e40
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-22 20:40:24 -0400

    refactor and connect

--- client/src/containers/DragQueens.js
+++ client/src/containers/DragQueens.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import DragQueenCard from '../components/DragQueenCard'
 import DragQueenForm from '../containers/DragQueenForm'
+
+import { getDragQueens } from '../actions/dragQueens'
+
 class DragQueens extends Component {
 
-  render (){
-    return(
-      <div>
-      <DragQueenForm />
-        <h2>Drag Queens:</h2>
-        { this.props.dragQueens.map(dragQueen => <DragQueenCard key={dragQueen.id} dragQueen={dragQueen} />)}
+  componentDidMount() {
+    this.props.getDragQueens()
+    console.log(this.props)
+  }
 
+  render() {
+    return (
+      <div>
+
+        <h2>Drag Queens:</h2>
+        {this.props.dragQueens.map(dragQueen => <DragQueenCard key={dragQueen.id} dragQueen={dragQueen} />)}
+  <DragQueenForm />
       </div>
     )
    }
   }
 
-export default DragQueens;
+  const mapStateToProps = (state) => {
+    return ({
+      dragQueens: state.dragQueens
+    })
+  }
+
+export default connect(mapStateToProps, { getDragQueens })(DragQueens);
