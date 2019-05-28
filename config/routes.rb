780eabe0669a3916ce2c4a390fda9a9d406b73cc
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-28 16:12:10 -0400

    remove drag_queen resources exceptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :drag_queens, except: [:new, :edit]
+    resources :drag_queens
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
