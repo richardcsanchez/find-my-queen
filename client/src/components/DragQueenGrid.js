9e3ddca976264539c171b8a63f63d770e4cda56a
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-29 19:38:36 -0400

    create drag queen grid presentational component

---
+++ client/src/components/DragQueenGrid.js
@@ -0,0 +1,14 @@
+import React, { component }  from 'react';
+import DragQueens from '../containers/DragQueens'
+
+class DragQueenGrid extends React.Component {
+  render() {
+    return (
+      <div>
+        <DragQueens />
+      </div>
+    )
+  }
+}
+
+export default DragQueenGrid
