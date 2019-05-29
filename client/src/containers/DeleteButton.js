cf2cc1182e2cc103d5d65617f9198926821cb6bd
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-29 16:59:50 -0400

    create container file for delete button

---
+++ client/src/containers/DeleteButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteDragQueen } from '../actions/dragQueens'
+
+class DeleteButton extends Component {
+
+  handleOnClick = () => {
+    this.props.deleteDragQueen(this.props.dragQueen.id)
+  }
+
+  render() {
+    return (
+        <button onClick={this.handleOnClick}>
+        Delete
+        </button>
+    )}
+}
+    const mapStateToProps = state => {
+      return {
+        dragQueens: state.dragQueens
+      }
+    }
+
+
+export default connect(mapStateToProps, { deleteDragQueen } )(DeleteButton);
