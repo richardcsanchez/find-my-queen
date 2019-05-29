bc04ca7d5306cb2a2fc1f82e4d8e5cbdd0ab7764
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-29 19:38:01 -0400

    remove extra imports and debug routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import './index.css';
-
 import store from './store.js';
-
-
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-  <App />
-  </Provider>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
