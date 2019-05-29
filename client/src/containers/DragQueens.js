67f1c2e2887bb2c22e590bdcd4d5d5ede916e655
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-29 14:40:23 -0400

    add sort method and image cards

--- client/src/containers/DragQueens.js
+++ client/src/containers/DragQueens.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import DragQueenImageCard from '../components/DragQueenImageCard'
+import DragQueenGrid from './DragQueenGrid'
 import DragQueenCard from '../components/DragQueenCard'
 import DragQueenForm from '../containers/DragQueenForm'
 
@@ -12,12 +14,24 @@ class DragQueens extends Component {
     this.props.getDragQueens()
   }
 
+  sortByName = (a, b) => {
+    if (a.name < b.name)
+      return -1;
+    if (a.name > b.name)
+      return 1
+    return 0
+  }
+
   render() {
     return (
       <div>
       <DragQueenForm />
+
         <h2>Drag Queens:</h2>
-        {this.props.dragQueens.map(dragQueen => <DragQueenCard key={dragQueen.id} dragQueen={dragQueen} />)}
+        <div class='gallery'>
+        {this.props.dragQueens.sort(this.sortByName).map(dragQueen =>
+          <DragQueenImageCard key={dragQueen.id} dragQueen={dragQueen} />)}
+          </div>
       </div>
     )
    }
