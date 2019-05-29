9f8a6c055902df98a33fa53904b9ead5903e61b3
Richard Sanchez <richard.charles.s@gmail.com>
2019-05-29 19:39:11 -0400

    add presentaional form component

---
+++ client/src/components/NewDragQueenForm.js
@@ -0,0 +1,15 @@
+import React, { component }  from 'react';
+import DragQueenForm from "../containers/DragQueenForm"
+
+class NewDragQueenForm extends React.Component {
+    render() {
+      return (
+        <div>
+          <DragQueenForm />
+        </div>
+      )
+    }
+  }
+
+
+export default NewDragQueenForm
