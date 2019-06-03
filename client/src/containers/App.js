680228eee34b1c658e50838ec7f05c3f2d1c9690
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-03 16:32:07 -0400

    import navbar component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import "./App.css"
+import 'react-bootstrap'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux'
 import DragQueenGrid from '../components/DragQueenGrid'
 import DragQueenCard from '../components/DragQueenCard'
 import NewDragQueenForm from '../components/NewDragQueenForm'
-
+import NavBar from '../components/NavBar'
+import Nav from 'react-bootstrap/Nav'
 
 
 class App extends Component {
@@ -13,13 +15,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Find My Queen!
         <Router>
+        <div>
+        <h1>Find My Queen!</h1>
+        <NavBar />
+
           <Switch>
           <Route exact path = '/drag_queens' component={DragQueenGrid} />
           <Route exact path = '/drag_queens/new' component={NewDragQueenForm} />
           <Route exact path = '/drag_queens/:id' component={DragQueenCard}/>
         </Switch>
+        </div>
       </Router>
 
       </div>
