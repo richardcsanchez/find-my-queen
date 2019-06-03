4c0dc3c30c3dd6a3681c943db18820466a2a4a3a
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-03 16:30:02 -0400

    add navbar with styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav'
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'pink',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const NavBar = () =>
+  <div>
+    <NavLink to="/drag_queens" exact style={link} activeStyle={{background: 'grey'}}>Home</NavLink>
+    <NavLink to="/drag_queens/new" exact style={link} activeStyle={{ background: 'grey' }}>New Queen</NavLink>
+  </div>;
+
+export default NavBar
