a6aaead36f529a0da4e25980f0df83b1e0926e9f
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-03 13:37:45 -0400

    debug resent form

--- client/src/reducers/dragQueenFormData.js
+++ client/src/reducers/dragQueenFormData.js
@@ -7,7 +7,7 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'RESET_FORM':
             return initialState;
 
