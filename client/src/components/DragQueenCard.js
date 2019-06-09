414b766a7984f1661333b9adf9b883c886d42d1f
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-08 23:13:15 -0400

    edit imports, stylistic updates

--- client/src/components/DragQueenCard.js
+++ client/src/components/DragQueenCard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect }  from 'react';
 import DeleteButton from '../containers/DeleteButton'
 import { BrowserRouter as Router,  Route, NavLink, Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom'
-
 import fetch from '../modules/fetch'
 
 function DragQueenCard({ match }) {
@@ -23,13 +22,16 @@ function DragQueenCard({ match }) {
   return(
     <div className='LargeDragQueenCard'>
       <br></br>
-      <h2>{dragQueen.name}</h2>
+        <h2>{dragQueen.name}</h2>
       <div className='DragQueenShowCard'>
-      <img className ='DragQueenImage' alt={ dragQueen.name } src= {dragQueen.img_url} />
+        <img className ='DragQueenImage' alt={ dragQueen.name } src= {dragQueen.img_url} />
       </div>
       <br></br>
+      <h4>Hometown:</h4>
       <p>{dragQueen.hometown}</p>
+      <h4>Drag Style:</h4>
       <p>{dragQueen.style}</p>
+      <h4>Bio:</h4>
       <p>{dragQueen.bio}</p>
       <button className='small-button'>
         <Link className='link'key={dragQueen.id} to={{pathname: '/drag_queens/' + dragQueen.id + '/edit' } }>Edit</Link>
