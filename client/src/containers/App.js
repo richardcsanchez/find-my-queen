bb046f9c2daa87f0ff2bb2c6a1ec0efadb95e4b7
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-08 21:15:06 -0400

    mount setDragQueens on higher component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
 import "./App.css"
 import 'react-bootstrap'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  Route,
+  Link,
+  Redirect,
+  Switch,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getDragQueens } from '../actions/dragQueens'
 import DragQueens from '../containers/DragQueens'
 import DragQueenCard from '../components/DragQueenCard'
 import NewDragQueenForm from '../components/NewDragQueenForm'
 import NavBar from '../components/NavBar'
 import EditDragQueen from './EditDragQueen.js'
+import EditingDragQueen from './EditingDragQueen'
 
 class App extends Component {
 
-
+  componentDidMount() {
+    this.props.getDragQueens()
+  }
   render() {
+    const { dragQueens, match } = this.props;
+
     return (
       <div className="App">
         <Router>
@@ -21,10 +34,10 @@ class App extends Component {
 
           <Switch>
           <Route exact path = '/drag_queens' component={DragQueens} />
-          <Route exact path = '/drag_queens/new' component={NewDragQueenForm} />
-          <Route path='/drag_queens/:id/edit' component={EditDragQueen} />
-          <Route exact path='/drag_queens/:id' component={DragQueenCard}/>
-
+          <Route  path ='/drag_queens/new' component={NewDragQueenForm} />
+          <Route  path='/drag_queens/:id/edit' component={EditDragQueen} />
+          <Route path='/drag_queens/:id' component={DragQueenCard} />
+          <Route render={() => (<div> Sorry, this page does not exist. </div>)} />
         </Switch>
         </div>
       </Router>
@@ -34,4 +47,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return ({
+    dragQueens: state.dragQueens
+  })
+}
+
+
+export default connect(mapStateToProps, {getDragQueens})(App)
