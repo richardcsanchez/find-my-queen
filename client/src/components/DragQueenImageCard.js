9f8cb2b27b7912e25f8adfce21a118a744c53de2
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-17 14:01:54 -0400

    updates for AddTips

--- client/src/components/DragQueenImageCard.js
+++ client/src/components/DragQueenImageCard.js
@@ -1,17 +1,20 @@
 import React  from 'react';
 import { BrowserRouter as Router,  Route, NavLink } from 'react-router-dom';
 import '../components/DragQueensStyling.css'
-
+import AddTips from './AddTips'
 const DragQueenImageCard = ({ dragQueen }) => {
   const urlBase = '/drag_queens'
 
+
   return (
+    <div className='outer-container'>
     <div className='DragQueenCard' >
     <NavLink to= {{pathname: urlBase + "/" + dragQueen.id}} >
-        <img className='DragQueenImage' src={dragQueen.img_url} alt={dragQueen.name} />
+    <img className='DragQueenImage' src={dragQueen.img_url} alt={dragQueen.name} />
         </NavLink>
-
-      </div>
+        </div>
+        <AddTips />
+    </div>
   )
 }
 
