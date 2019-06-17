929e1397ba53b16d285782938b9481a63e029983
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-17 14:01:02 -0400

    add AddTips component

---
+++ client/src/components/AddTips.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class AddTips extends Component {
+
+  state = {
+    tips: 0
+  }
+
+  addSmallTip = () => {
+    this.setState(({ tips }) => ({
+      tips: tips + 1
+    }))
+  }
+  
+  addMediumTip = () => {
+    this.setState(({ tips }) => ({
+      tips: tips + 3
+    }))
+  }
+
+  addLargeTip = () => {
+    this.setState(({ tips }) => ({
+      tips: tips + 5
+    }))
+  }
+
+  render(){
+    return (
+      <div>
+      Add a Tip:
+      <br></br>
+      <button className = "tip-button" onClick={this.addSmallTip}>
+       $1
+      </button>
+      <button className = "tip-button" onClick={this.addMediumTip}>
+      $3
+        </button>
+        <button className = "tip-button" onClick={this.addLargeTip}>
+        $5
+          </button>
+          <br></br>
+          Total Tips: ${this.state.tips}
+      </div>
+    )
+  }
+}
+
+export default AddTips
