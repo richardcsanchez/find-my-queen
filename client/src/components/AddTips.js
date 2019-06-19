25f6c0ac74fb37ceb522d2fedcffdbff38ceb946
Richard Sanchez <richard.charles.s@gmail.com>
2019-06-19 12:01:35 -0400

    refactor add tips

--- client/src/components/AddTips.js
+++ client/src/components/AddTips.js
@@ -2,26 +2,13 @@ import React, { Component } from 'react';
 
 class AddTips extends Component {
 
-  state = {
-    tips: 0
+  constructor(props){
+    super(props)
+    this.handleAddTip=this.handleAddTip.bind(this)
   }
 
-  addSmallTip = () => {
-    this.setState(({ tips }) => ({
-      tips: tips + 1
-    }))
-  }
-
-  addMediumTip = () => {
-    this.setState(({ tips }) => ({
-      tips: tips + 3
-    }))
-  }
-
-  addLargeTip = () => {
-    this.setState(({ tips }) => ({
-      tips: tips + 5
-    }))
+  handleAddTip(e){
+    this.props.onAddTip(e.target.value)
   }
 
   render(){
@@ -29,17 +16,11 @@ class AddTips extends Component {
       <div>
         Add a Tip:
           <br></br>
-        <button className = "tip-button small-button" onClick={this.addSmallTip}>
+        <button className = "tip-button small-button"
+          onClick={this.handleAddTip}>
           $1
         </button>
-        <button className = "tip-button small-button" onClick={this.addMediumTip}>
-          $3
-        </button>
-        <button className = "tip-button small-button" onClick={this.addLargeTip}>
-          $5
-        </button>
           <br></br>
-        Total Tips: ${this.state.tips}
       </div>
     )
   }
--- client/src/components/DragQueenImageCard.js
+++ client/src/components/DragQueenImageCard.js
@@ -3,20 +3,42 @@ import { BrowserRouter as Router,  Route, NavLink } from 'react-router-dom';
 import '../components/DragQueensStyling.css'
 import AddTips from './AddTips'
 
-const DragQueenImageCard = ({ dragQueen }) => {
-  const urlBase = '/drag_queens'
+class DragQueenImageCard extends React.Component {
 
 
+   constructor(props) {
+     super(props);
+
+       this.state = {
+       tips: 0
+     }
+     this.handleAddTip=this.handleAddTip.bind(this)
+   }
+
+  handleAddTip(tip) {
+    this.setState(({ tips }) => ({
+      tips: tips + 1
+    }))
+    console.log(this.props.dragQueen)
+  }
+
+  render(){
+    const urlBase = '/drag_queens'
   return (
       <div className='outer-container'>
         <div className='DragQueenCard' >
-          <NavLink dragQueen= {dragQueen} to= {{pathname: urlBase + "/" + dragQueen.id}} >
-            <img className='DragQueenImage' src={dragQueen.img_url} alt={dragQueen.name} />
+          <NavLink dragQueen= {this.props.dragQueen} to= {{pathname: urlBase + "/" + this.props.dragQueen.id}} >
+            <img className='DragQueenImage' src={this.props.dragQueen.img_url} alt={this.props.dragQueen.name} />
           </NavLink>
         </div>
-      <AddTips />
+      <AddTips
+      tips={this.state.tips}
+      onAddTip={this.handleAddTip}
+      dragQueen={this.props.dragQueen}
+       />
+      Total Tips: ${this.state.tips}
     </div>
   )
 }
-
+}
 export default DragQueenImageCard;
