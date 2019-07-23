792704d0557b7e4a84c7b75431da9852e715a26a
Richard Sanchez <richard.charles.s@gmail.com>
2019-07-23 13:49:00 -0400

    add search bar and state

--- client/src/containers/DragQueens.js
+++ client/src/containers/DragQueens.js
@@ -3,8 +3,22 @@ import { connect } from 'react-redux';
 import '../components/DragQueensStyling.css'
 import { getDragQueens } from '../actions/dragQueens'
 import DragQueenGrid from '../components/DragQueenGrid'
+import SearchBar from '../components/SearchBar'
 
 export class DragQueens extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        filterText: ""
+      }
+      this.handleFilterTextChange=this.handleFilterTextChange.bind(this)
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    })
+  }
 
   componentDidMount() {
     this.props.getDragQueens()
@@ -14,8 +28,12 @@ export class DragQueens extends Component {
     return (
       <div>
         <h2>Drag Queens:</h2>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}/>
+
           <div className='DragQueensContainer'>
-            <DragQueenGrid dragQueens={this.props.dragQueens} />
+            <DragQueenGrid dragQueens={this.props.dragQueens} filterText={this.state.filterText}/>
           </div>
       </div>
     )
