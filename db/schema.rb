c3f6252d746a43c0a03a97defc6082ec2910f24a
Richard Sanchez <richard.charles.s@gmail.com>
2019-08-27 15:20:08 -0400

    revert to pre-postgres merge

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_05_22_202004) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "cities", force: :cascade do |t|
     t.string "city"
     t.datetime "created_at", null: false
